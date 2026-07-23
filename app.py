06e634ca85ee219f84ddac6aa270a85688f3467b
Billgint05 <69377975+Billgint05@users.noreply.github.com>
2026-07-23 14:57:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(
-    page_title="Sales Dashboard",
-    layout="wide"
-)
+DAILYSALES_URL = "PASTE_LINK"
 
-# GANTI NANTI DENGAN LINK SHAREPOINT
-DAILYSALES_URL = "https://inovapharma.sharepoint.com/:x:/s/Indonesia/IQBSk90CCW1qTaqGADTBFrfeAVLOChDuyW2pXTGYy1I1-kU?e=uhsLLD"
-
-st.title("📈 Sales Dashboard")
-
-@st.cache_data
-def load_sales():
-    return pd.read_excel(DAILYSALES_URL)
-
-df = load_sales()
-
-st.success(f"Loaded {len(df):,} rows")
-
-st.dataframe(df.head(100))
+st.write(DAILYSALES_URL)
