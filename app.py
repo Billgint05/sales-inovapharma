c4fc019fd1f84accfcf7020309c619e0aa3927b6
Billgint05 <69377975+Billgint05@users.noreply.github.com>
2026-07-23 15:08:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,10 @@
 import streamlit as st
 import pandas as pd
-import requests
-from io import BytesIO
 
-st.set_page_config(
-    page_title="Sales Dashboard",
-    layout="wide"
-)
+st.title("Sales Dashboard")
 
-st.title("📈 Sales Dashboard")
+df = pd.read_excel("DailySales_APL.xlsx")
 
-# GANTI DENGAN LINK SHAREPOINT COPY LINK
-DAILYSALES_URL = "https://inovapharma.sharepoint.com/:x:/s/Indonesia/IQBSk90CCW1qTaqGADTBFrfeAVLOChDuyW2pXTGYy1I1-kU?e=uhsLLD"
+st.write(df.shape)
 
-
-def make_download_url(url):
-    if "?" in url:
-        return url + "&download=1"
-    else:
-        return url + "?download=1"
-
-
-@st.cache_data
-def load_sales():
-    download_url = make_download_url(DAILYSALES_URL)
-
-    response = requests.get(download_url)
-
-    if response.status_code != 200:
-        st.error(f"Gagal baca SharePoint. Status code: {response.status_code}")
-        st.stop()
-
-    return pd.read_excel(BytesIO(response.content))
-
-
-df = load_sales()
-
-st.success(f"Loaded {len(df):,} rows")
-
-st.write("Columns:")
-
-st.dataframe(
-    pd.DataFrame({
-        "Column Name": df.columns
-    })
-)
-
-st.dataframe(df.head(100))
+st.dataframe(df.head(20))
