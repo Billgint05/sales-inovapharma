18cdc8ec77be5586d5b852087367dec470f084b4
Billgint05 <69377975+Billgint05@users.noreply.github.com>
2026-07-23 15:19:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,43 @@ st.set_page_config(
 
 fact = pd.read_excel("DailySales_APL.xlsx")
 
-outlet = pd.read_csv("IMS Mapping - Outlet.csv")
+outlet = pd.read_csv(
+    "IMS Mapping - Outlet.csv",
+    dtype=str
+)
 
-sku = pd.read_csv("IMS Mapping - SKU.csv")
+sku = pd.read_csv(
+    "IMS Mapping - SKU.csv",
+    dtype=str
+)
+
+# ======================
+# FORMAT KEY
+# ======================
+
+fact["CUSTOMER_CODE"] = (
+    fact["CUSTOMER_CODE"]
+    .astype(str)
+    .str.strip()
+)
+
+outlet["Distributor Customer ID"] = (
+    outlet["Distributor Customer ID"]
+    .astype(str)
+    .str.strip()
+)
+
+fact["ZP_ITEM_CODE"] = (
+    fact["ZP_ITEM_CODE"]
+    .astype(str)
+    .str.strip()
+)
+
+sku["DIST_SKU CODE"] = (
+    sku["DIST_SKU CODE"]
+    .astype(str)
+    .str.strip()
+)
 
 # ======================
 # MERGE OUTLET
@@ -40,8 +74,40 @@ fact = fact.merge(
 
 df = fact
 
+# ======================
+# DASHBOARD
+# ======================
+
 st.title("📈 Sales Performance Dashboard")
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.metric(
+        "Rows",
+        f"{len(df):,}"
+    )
+
+with col2:
+    st.metric(
+        "Branch",
+        f"{df['BRANCH'].nunique():,}"
+    )
+
+with col3:
+    st.metric(
+        "Invoice",
+        f"{df['INVOICE_NO'].nunique():,}"
+    )
+
+with col4:
+    st.metric(
+        "SKU",
+        f"{df['ZP_ITEM_CODE'].nunique():,}"
+    )
+
+st.write("Data Shape")
 
 st.write(df.shape)
 
-st.dataframe(df.head(20))
+st.dataframe(df.head(100))
