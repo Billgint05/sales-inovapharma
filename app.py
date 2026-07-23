7ac63f5426635011679be6919f83289cb6978aad
Billgint05 <69377975+Billgint05@users.noreply.github.com>
2026-07-23 15:00:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Billy Test")
+st.set_page_config(
+    page_title="Sales Dashboard",
+    layout="wide"
+)
 
-st.write("Hello World")
+st.title("📈 Sales Dashboard")
+
+df = pd.read_excel("DailySales_APL.xlsx")
+
+st.success(f"Loaded {len(df):,} rows")
+
+st.write("Columns:")
+
+st.dataframe(
+    pd.DataFrame({
+        "Column Name": df.columns
+    })
+)
+
+st.dataframe(df.head(20))
